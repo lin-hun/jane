98a0617eb96b43f72550522796b0dc53cfebf24f
fusheng <fusheng002@lianjia.com>
2018-02-25 17:38:26 +0800

    feat:up build

--- cli/lib/build.js
+++ cli/lib/build.js
@@ -3,3 +3,33 @@
 // solve npm require
 
 // copy to build dir
+let fs = require('fs')
+let path = require('path')
+let jsCompile = require('./compile-js')
+let cssCompile = require('./compile-css')
+
+function ignoreMatch(target){
+
+}
+
+function walk(target){
+  if(fs.lstatSync(target).isFile()){
+    // ignore files in config.js
+    if('css'){
+      cssCompile(target)
+    }
+    if('js'){
+      jsCompile(target)
+    }
+    else{
+      // copy
+    }
+    return
+  }
+  if(fs.lstatSync(target).isDirectory()){
+    fs.readdirSync(target).forEach(v=>{
+      walk(path.join(target,v))
+    })
+  }
+}
+walk(path.join(__dirname,'../../cli'))
