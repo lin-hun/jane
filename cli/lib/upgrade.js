87071534262b3c781f54b55dddc33ca0afe4426f
fusheng <fusheng002@lianjia.com>
2018-02-25 13:41:42 +0800

    feat:enhancement

--- cli/lib/upgrade.js
+++ cli/lib/upgrade.js
@@ -1,19 +1,20 @@
 let fetch = require('node-fetch')
-let localV = require('../package').version
-let log = require('../utils/utils').log
+let utils = require('../utils/utils')
+let localV = utils.getVer()
+let log = utils.log
 let child_process = require('child_process');
 
-(async function(){
+(async function () {
   // search on npm
   let res = await fetch('https://registry.npmjs.com/@linhun%2Fjane-cli').then(res => res.json())
   let onlineV = res['dist-tags'].latest
   // diff
-  if(localV < onlineV){
+  if (localV < onlineV) {
     log.info(`线上最新为${onlineV},升级中...`)
     log.info(`请输入密码，赋予读写权限`)
-    child_process.execSync("sudo npm install @linhun/jane-cli -g",{stdio:[0,1,2]})
+    child_process.execSync("sudo npm install @linhun/jane-cli -g", {stdio: [0, 1, 2]})
   }
-  else{
+  else {
     log.info('已是最新版')
   }
 })()
