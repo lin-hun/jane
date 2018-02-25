cb84b75c0ab2c6e716e06a088ad8207eb6d89a30
fusheng <fusheng002@lianjia.com>
2018-02-25 17:38:10 +0800

    feat:up config rules

---
+++ cli/tpl/project/jane.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dest:'build',
+  css: {
+    ext:'scss'
+    config: {}
+  },
+  js: {
+    compiler: 'babel',
+    config: {}
+  },
+  ignore:['node_modules','dist','.DB_store','.DS_Store']
+}
