1b92e1767741528613ffad5f914c2dc59253291f
fusheng <fusheng002@lianjia.com>
2018-02-26 15:47:45 +0800

    feat:up js compile

--- cli/lib/compile-js.js
+++ cli/lib/compile-js.js
@@ -1,3 +1,23 @@
-module.exports = ()=>{
+let compiler = require('babel-core')
+let fs = require('fs')
+let utils = require('../utils/utils')
+let config = utils.getProjectConfig().js.config
+let log = utils.log
 
+// option:https://github.com/sass/node-sass
+module.exports = (path) => {
+  compiler.transformFile(path,config, (err, result) => {
+    if (err) {
+      log.error(err)
+      return
+    }
+    // compile to .js
+    path = utils.getOutputFile(path)
+    // write in
+    utils.write(path, result.code).then(v => {
+      log.tag('写入',`${utils.getOutputFile(path)}`)
+    }).catch((err) => {
+      log.error(err)
+    })
+  })
 }
